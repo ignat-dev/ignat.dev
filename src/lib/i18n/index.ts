8a3f04b32464ed598f8a227f3ffd0f351932c323
Ignat Ignatov <i@ignat.dev>
2025-09-21 23:07:27 +0300

    Improve i18n library.

--- src/lib/i18n/index.ts
+++ src/lib/i18n/index.ts
@@ -2,6 +2,8 @@ export const SUPPORTED_LOCALES: Array<string> = [ "en" ]
 
 export const DEFAULT_LOCALE: typeof SUPPORTED_LOCALES[number] = "en"
 
+type ArgType = string | number | ((key: string) => string)
+
 export async function useTranslations(lang: typeof SUPPORTED_LOCALES[number] = DEFAULT_LOCALE) {
   const { default: locale } = (
     SUPPORTED_LOCALES.includes(lang)
@@ -9,7 +11,7 @@ export async function useTranslations(lang: typeof SUPPORTED_LOCALES[number] = D
       : { default: {} }
   )
 
-  return function t(key: string, ...args: Array<string | Record<string, string>>): string {
+  return function t<T = ArgType>(key: string, ...args: Array<T | Record<string, T>>): string {
     let result = key.split(".").reduce((r, x) => r?.[x], locale)
 
     if (result && args.length) {
@@ -20,18 +22,34 @@ export async function useTranslations(lang: typeof SUPPORTED_LOCALES[number] = D
   }
 }
 
-function replaceAllPlaceholders(value: string, args: Array<string | Record<string, string>>): string {
+function replaceAllPlaceholders<T>(value: string, args: Array<T | Record<string, T>>): string {
   return (
     typeof args[0] === "object"
-      ? replaceNamedPlaceholders(value, args[0] as Record<string, string>)
-      : replaceIndexedPlaceholders(value, args as Array<string>)
+      ? replaceNamedPlaceholders<T>(value, args[0] as Record<string, T>)
+      : replaceIndexedPlaceholders<T>(value, args as Array<T>)
   )
 }
 
-function replaceIndexedPlaceholders(value: string, args: Array<string>): string {
-  return value.replace(/\{\{(\d+)\}\}/g, (m: string, i: number) => args[i] ?? m)
+function replaceIndexedPlaceholders<T>(value: string, args: Array<T>): string {
+  return value.replace(/\{\{(\d+)\}\}/g, (m: string, i: number) => getPlaceholderValue(args[i], i) ?? m)
 }
 
-function replaceNamedPlaceholders(value: string, dict: Record<string, string>): string {
-  return value.replace(/\{\{(\w+)\}\}/g, (m: string, k: string) => dict[k] ?? m)
+function replaceNamedPlaceholders<T>(value: string, dict: Record<string, T>): string {
+  return value.replace(/\{\{(\w+)\}\}/g, (m: string, k: string) => getPlaceholderValue(dict[k], k) ?? m)
+}
+
+function getPlaceholderValue<T>(arg: T, key: string | number): string {
+  if (typeof arg === "string") {
+    return arg
+  }
+
+  if (typeof arg === "number") {
+    return arg.toString()
+  }
+
+  if (typeof arg === "function") {
+    return arg(`${key}`)?.toString() ?? ""
+  }
+
+  return ""
 }
