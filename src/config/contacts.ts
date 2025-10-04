68c9167c13f4d87f48f31d60a754591f70f59677
Ignat Ignatov <i@ignat.dev>
2025-10-04 20:32:28 +0300

    Add contact section.

---
+++ src/config/contacts.ts
@@ -0,0 +1,19 @@
+import type { LinkData } from "@types"
+
+export const contacts: Array<LinkData> = [
+  {
+    icon: "/icons/email.svg",
+    link: "mailto:hello@ignat.dev",
+    text: "Email",
+  },
+  {
+    icon: "/icons/linkedin.svg",
+    link: "https://linkedin.com/in/ignat-dev",
+    text: "LinkedIn",
+  },
+  {
+    icon: "/icons/github.svg",
+    link: "https://github.com/ignat-dev",
+    text: "GitHub",
+  }
+]
