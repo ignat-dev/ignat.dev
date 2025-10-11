2c2ce04d52e4e951df8b354122b848f45330a790
Ignat Ignatov <i@ignat.dev>
2025-10-11 17:25:48 +0300

    Unify components props and usage.

---
+++ src/types/ComponentProps.ts
@@ -0,0 +1,5 @@
+export interface ComponentProps {
+  class?: string
+  id?: string
+  theme?: "light" | "dark"
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
+export type { ComponentProps } from "./ComponentProps"
 export type { LinkData } from "./LinkData"
 export type { NavItem } from "./NavItem"
 export type { TechGroup } from "./TechGroup"
